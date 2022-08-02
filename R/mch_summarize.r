a0aae87ee6c1c9c117b84a174c03fa2c8dd905f1
Adam Smith <asmith@mobot.org>
2022-08-01 23:31:57 -0500

    Can't touch this

--- R/mch_summarize.r
+++ R/mch_summarize.r
@@ -4,15 +4,15 @@
 #'
 #' @param mcmc	An object of class \code{mcmc} or \code{mcmc.list}, or a \code{list}. If a \code{list}, the function searches down the first element to see if it can find an \code{mcmc} or \code{mcmc.list} object.
 #' @param fun A function that generates the summary statistic. This is the actual function, not the "name" of the function. For example, \code{mean}, not \code{"mean"}. The default is to calculate the mean.  The function is applied across all values of the variable across all chains at once. You can supply additional arguments to the function in \code{...}.
-#' @param param Name of the variable(s). The outcome depends on the definitions of \code{i}, \code{j}, \code{k}, and \code{l}. Please see the help for \code{\link{emc_param}} for more explanation on how to specify this argument, plus \code{i}, \code{j}, \code{k}, and \code{l}.
+#' @param param Name of the variable(s). The outcome depends on the definitions of \code{i}, \code{j}, \code{k}, and \code{l}. Please see the help for \code{\link{mch_param}} for more explanation on how to specify this argument, plus \code{i}, \code{j}, \code{k}, and \code{l}.
 #' @param i,j,k,l Indices used to specify variable names.
-#' @param mcmc_stacked \code{FALSE} (default), in which case \code{mcmc} is assumed to be an object of class \code{mcmc}, \code{mcmc.list}, or a \code{list}, or \code{TRUE}, in which case it is a "stacked" MCMC table. This argument is usually used by other functions in this package, so can often be ignored. However, if your MCMC chains have a lot of iterations or variables, then you can speed things up by "stacking" the chains using \code{\link{emc_stack}}, then using that for \code{mcmc}.
+#' @param mcmch_stacked \code{FALSE} (default), in which case \code{mcmc} is assumed to be an object of class \code{mcmc}, \code{mcmc.list}, or a \code{list}, or \code{TRUE}, in which case it is a "stacked" MCMC table. This argument is usually used by other functions in this package, so can often be ignored. However, if your MCMC chains have a lot of iterations or variables, then you can speed things up by "stacking" the chains using \code{\link{mch_stack}}, then using that for \code{mcmc}.
 #' @param ... Arguments to pass to a function named in \code{fun}.
 #'
 #' @return A named numeric vector.
 #' @export
 
-emc_summarize <- function(
+mch_summarize <- function(
 	mcmc,
 	fun = mean,
 	param = NULL,
@@ -20,22 +20,22 @@ emc_summarize <- function(
 	j = NULL,
 	k = NULL,
 	l = NULL,
-	mcmc_stacked = FALSE,
+	mcmch_stacked = FALSE,
 	...
 ) {
 
 	# compile MCMC
-	if (!mcmc_stacked) mcmc <- emc_stack(mcmc)
+	if (!mcmch_stacked) mcmc <- mch_stack(mcmc)
 	
 	# get variables
-	param <- emc_param(
+	param <- mch_param(
 		param = param,
 		i = i,
 		j = j,
 		k = k,
 		l = l,
 		mcmc = mcmc,
-		mcmc_stacked = TRUE
+		mcmch_stacked = TRUE
 	)
 	
 	
--- R/mcmcHammer.r
+++ R/mcmcHammer.r
@@ -1,21 +1,21 @@
-#' emcemc: Analysis of MCMC chains
+#' mcmcHammer: Analysis of MCMC chains
 #'
 #' Helper functions for analyzing MCMC chains. Includes trace plots and density plots. This package automates extraction of specific parameters, including indexed coefficients such as "beta[1, 1]", "beta[1, 2]", "beta[1, 3]", and similar.
 #'
-#' Create an issue on \href{https://github.com/adamlilith/enmemc/issues}{GitHub}.
+#' Create an issue on \href{https://github.com/adamlilith/mcmcHammer/issues}{GitHub}.
 #'
 #' @details
 #' @section MCMC diagnostics:
-#' 		\code{\link{emc_trace_dens}} Trace plots and density plots \cr
+#' 		\code{\link{emch_trace_dens}} Trace plots and density plots \cr
 #' 
 #' @section MCMC chain manipulation:
-#' 		\code{\link{emc_stack}} "Stack" multiple MCMC chains on one another \cr
+#' 		\code{\link{emch_stack}} "Stack" multiple MCMC chains on one another \cr
 #'
 #' @section Helper functions:
-#' 		\code{\link{emc_param}} Create variable names (e.g., with indices) \cr
+#' 		\code{\link{emch_param}} Create variable names (e.g., with indices) \cr
 #' 
 #' @docType package
 #' @author Adam B. Smith
-#' @name emcemc
+#' @name mcmcHammer
 #' @keywords internal
 NULL
