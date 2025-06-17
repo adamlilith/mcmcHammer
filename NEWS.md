f7f6b30fb744b1ab9a365e020555d4af800ff057
Adam B. Smith <adam.smith@mobot.org>
2025-06-16 22:11:31 -0500

    Inherited params

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,8 @@
+# mcmHammer 2.0.1 (2025-06-16)
+o `hammer_subset()` now allows selecting multiple parameters with different index patterns using the `indices` argument.  
+o Fix bug in `hammer_summarize()` where `$summary` tag was left out and summaries put into top-level element.  
+o Esoteric for developer: Shared `param` definitions.  
+
 # mcmHammer 2.0.0 (2025-06-06)
 
 Complete package re-vamp. Removed plotting functions because redundant with other packages. Package now focuses on MCMC chain and chain-like object manipulations like subsetting, extraction, and `cbind()`ing and `rbind()`ing chains.
--- R/hammer_param.r
+++ R/hammer_param.r
@@ -6,15 +6,8 @@
 #' * `param = NULL`: All variables in the MCMC object. An object must be supplied to the `mcmc` argument.
 #' * `param =` a character vector and any of `i`, `j`, `k`, and/or `l` are not `NULL`: Returns a vector of variable names dependent on which indices are not `NULL`. You define `i`, `j`, `k`, and/or `l` as numeric vectors. For example, `hammer_param('beta', i = 1:3)` will return `beta1`, `beta2`, and `beta3`. You can also define any index as `TRUE` or `FALSE` (they have the same effect), in which case the function will return the names of all variables in the `mcmc` object that match the given index.
 #'
-#' @param i,j,k,l Indices used to specify variable names:
-#' * `i` refers to indices that occur immediately after the variable's "base" name. For example, if `param` is `beta`, and `i = 0:2', then the function will return `beta0`, `beta1`, and `beta2`.
-#'  * `j` refers to the *first* index in square brackets. For example, if `param` is `beta` and `j = 1:3`, then the output will be \code{beta[1]}, \code{beta[2]}, and \code{beta[3]}.
-#'  * `k` refers to the *second* index in square brackets. If `k` is not `NULL`, then `j` cannot be `NULL`. For example, if `param` is `beta`, `j = 1:2`, and `k = 1:3`, then the output will be like \code{beta[1, 1]}, \code{beta[2, 1]}, \code{beta[1, 2]}, \code{beta[2, 2]}, \code{beta[1, 3]}, and \code{beta[2, 3]}.
-#' * `l` refers to the *third* index in square brackets. You must also define `j` and `k` is you provide `l`. For example, if `param` is `beta`, `j = 1:2`, `k = 4:5`, and `l = 9:10`, then the output will be like \code{beta[1, 4, 9]}, \code{beta[2, 4, 9]}, \code{beta[1, 5, 9]}, \code{beta[2, 5, 9]}, \code{beta[1, 4, 10]}, \code{beta[2, 4, 10]}, and so on.
-#' * You can also define `i` when you define `j` alone, `j` and `k`, or `j`, `k`, and `l` to get outputs like \code{beta0[1]}`, or like \code{beta0[1, 1]}, or like \code{beta0[1, 2, 3]}`.
-#' You must specify all of the indices that would appear in square brackets. For example, for the pattern \code{param[j, k, l]}, you need to define `j`, `k`, and `l`. You can't just define `k = 1:2`. You need to set `j` and `l` equal to numeric/integer vectors and/or logical values.
-#'
-#' @param mcmc	An object of class `mcmc` or `mcmc.list`, *or* a `list`. If a `list`, the function searches down the first element to see if it can find an `mcmc` or `mcmc.list` object, then uses this if it can.
+#' @inheritParams .ijkl
+#' @inheritParams .mcmc
 #'
 #' @returns Character vector of variables.
 #' @examples
