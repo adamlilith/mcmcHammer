fe830b63bf5ffd019189ed6aec2294ff5b665199
Adam B. Smith <adam.smith@mobot.org>
2025-11-29 03:32:10 -0600

    Update hammer_subset.r

--- R/hammer_subset.r
+++ R/hammer_subset.r
@@ -11,6 +11,8 @@
 #'
 #' @param keep Logical: If `TRUE` (default), columns with names in `param` are retained. If `FALSE`, they are discarded. This cannot be set to `FALSE` when the `indices` argument is used.
 #'
+#' @param na.rm Logical: If `FALSE` (default), then summary statistics will be `NA` if any value in any iteration is `NA` (and it probably should be `NA`). However, if you set this to `TRUE`, then `NA` values are ignored.
+#'
 #' @returns An `mcmc` matrix, an `mcmc.list`, or  a list of list, one of which is an `mcmc.list`.
 #'
 #' @examples
@@ -37,7 +39,8 @@ hammer_subset <- function(
 	k = NULL,
 	l = NULL,
 	indices = NULL,
-	keep = TRUE
+	keep = TRUE,
+	na.rm = FALSE
 ) {
 
 	if (FALSE) {
@@ -55,6 +58,7 @@ hammer_subset <- function(
 
 	if (any(c(!is.null(i), !is.null(j), !is.null(k), !is.null(l))) & !is.null(indices)) stop('You cannot use `i`, `j`, `k`, or `l` *and* `indices`.')
 
+	### using `indices`
 	if (!is.null(indices)) {
 	
 		if (!keep) stop('The value of `keep` cannot be `FALSE` when `indices` is used.')
@@ -78,14 +82,20 @@ hammer_subset <- function(
 	mcmc_samples <- hammer_samples(mcmc)
 	mcmc_summaries <- hammer_summaries(mcmc, fail = FALSE)
 
-	params <- hammer_param(param, i = i, j = j, k = k, l = l, mcmc)
+	if (!inherits(mcmc_samples, 'mcmc.list')) mcmc_samples <- as.mcmc.list(mcmc_samples)
 
-	n_chains <- length(mcmc_samples)
+	params <- hammer_param(param, i = i, j = j, k = k, l = l, mcmc = mcmc)
+
+	n_chains <- hammer_n_chains(mcmc_samples)
 	cnames <- colnames(mcmc_samples[[1]])
 	for (n_chain in 1:n_chains) {
 		if (keep) {
 
-			this_chain <- mcmc_samples[[n_chain]]
+			this_chain <- if (inherits(mcmc_samples, c('mcmc.list', 'list'))) {
+				mcmc_samples[[n_chain]]
+			} else if (inherits(mcmc_samples, 'mcmc')) {
+				mcmc_samples
+			}
 			this_chain <- as.matrix(this_chain)
 			this_chain <- this_chain[ , params, drop = FALSE]
 
@@ -101,15 +111,16 @@ hammer_subset <- function(
 	}
 
 	if (!is.null(mcmc_summaries)) {
-		n <- length(mcmc_summaries)
-		cnames <- colnames(mcmc_samples[[1]])
-		for (n_chain in 1:n) {
-			if (keep) {
-				mcmc_summaries[[n_chain]] <- mcmc_summaries[[n_chain]][params, , drop = FALSE]
-			} else {
-				mcmc_summaries[[n_chain]] <- mcmc_summaries[[n_chain]][!(cnames %in% params), , drop = FALSE]
-			}
-		}
+		mcmc_summaries <- hammer_resummarize(mcmc_samples, na.rm = na.rm)
+		# n <- length(mcmc_summaries)
+		# cnames <- colnames(mcmc_samples[[1]])
+		# for (n_chain in 1:n) {
+		# 	if (keep) {
+		# 		mcmc_summaries[[n_chain]] <- mcmc_summaries[[n_chain]][params, , drop = FALSE]
+		# 	} else {
+		# 		mcmc_summaries[[n_chain]] <- mcmc_summaries[[n_chain]][!(cnames %in% params), , drop = FALSE]
+		# 	}
+		# }
 	}
 
 	list(
